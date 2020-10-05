f5d330a5a19a57207a06affaf57c21d0cced132a
Aditya Verma <aaadarths2357@gmail.com>
2020-10-05 20:45:36 +0530

    Refactor 2: Generic class for 3 generic type variables to find maximum

--- src/main/java/com/bridgelabz/maximumprogram/Maximum.java
+++ src/main/java/com/bridgelabz/maximumprogram/Maximum.java
@@ -2,15 +2,24 @@ package com.bridgelabz.maximumprogram;
 
 import java.lang.Comparable;
 
-public class Maximum {
+public class Maximum<T> {
+	T a;
+	T b;
+	T c;
 
-	public <T extends Comparable<T>> T findMaximum(T a, T b, T c) {
+	public Maximum(T a, T b, T c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 
-		T maxInput = a;
-		if (b.compareTo(maxInput) > 0)
-			maxInput = b;
-		if (c.compareTo(maxInput) > 0)
-			maxInput = c;
+	public static <T extends Comparable<T>> T findMaximum(Maximum<T> max) {
+
+		T maxInput = max.a;
+		if ((max.b).compareTo(maxInput) > 0)
+			maxInput = max.b;
+		if ((max.c).compareTo(maxInput) > 0)
+			maxInput = max.c;
 		return maxInput;
 	}
 
--- src/test/java/com/bridgelabz/maximumprogram/MaximumTest.java
+++ src/test/java/com/bridgelabz/maximumprogram/MaximumTest.java
@@ -9,54 +9,55 @@ import junit.framework.Assert;
 
 public class MaximumTest {
 
-	Maximum max;
 	
-	@Before
-	public void initialize() {
-		max=new Maximum();
-	}
-
-	
-
 //INTEGER TEST	
 	@Test
 	public void givenMaxNumber_At1stPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(3), max.findMaximum(3, 2, 1));	
+		Maximum<Integer> max=new Maximum<Integer>(3, 2, 1);
+		Assert.assertEquals(new Integer(3), max.findMaximum(max));	
 	}	
 	@Test
 	public void givenMaxNumber_At2ndPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(4), max.findMaximum(3, 4, 1));		
+		Maximum<Integer> max=new Maximum<Integer>(3, 4, 1);
+		Assert.assertEquals(new Integer(4), max.findMaximum(max));		
 	}
 	@Test
 	public void givenMaxNumber_At3rdPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(5), max.findMaximum(3,4,5));		
+		Maximum<Integer> max=new Maximum<Integer>(3,4,5);
+		Assert.assertEquals(new Integer(5), max.findMaximum(max));		
 	}
 	
 //FLOAT TEST	
 	@Test
 	public void givenMaxFloat_At1stPosition_ReturnFloat() {
-		Assert.assertEquals(new Float(3f), max.findMaximum(3f,2f,1f));		
+		Maximum<Float> max=new Maximum<Float>(3f,2f,1f);
+		Assert.assertEquals(new Float(3f), max.findMaximum(max));		
 	}	
 	@Test
 	public void givenMaxFloat_At2ndPosition_ReturnFloat() {
-		Assert.assertEquals(new Float(4f), max.findMaximum(3f,4f,1f));		
+		Maximum<Float> max=new Maximum<Float>(3f,4f,1f);
+		Assert.assertEquals(new Float(4f), max.findMaximum(max));		
 	}
 	@Test
 	public void givenMaxFloat_At3Position_ReturnFloat() {
-		Assert.assertEquals(new Float(5f), max.findMaximum(3f,4f,5f));		
+		Maximum<Float> max=new Maximum<Float>(3f,4f,5f);
+		Assert.assertEquals(new Float(5f), max.findMaximum(max));		
 	}
 
 //STRING TEST
 	@Test
 	public void givenMaxString_At1stPosition_ReturnString() {
-		Assert.assertEquals(new String("Cherries"), max.findMaximum("Cherries","Banana","Apple"));		
+		Maximum<String> max=new Maximum<String>("Cherries","Banana","Apple");
+		Assert.assertEquals(new String("Cherries"), max.findMaximum(max));		
 	}	
 	@Test
 	public void givenMaxString_At2ndPosition_ReturnString() {
-		Assert.assertEquals(new String("Grapefruit"), max.findMaximum("Cherries","Grapefruit","Apple"));		
+		Maximum<String> max=new Maximum<String>("Cherries","Grapefruit","Apple");
+		Assert.assertEquals(new String("Grapefruit"), max.findMaximum(max));		
 	}
 	@Test
 	public void givenMaxString_At3rdPosition_ReturnString() {
-		Assert.assertEquals(new String("Kiwi"), max.findMaximum("Cherries","Banana","Kiwi"));		
+		Maximum<String> max=new Maximum<String>("Cherries","Banana","Kiwi");
+		Assert.assertEquals(new String("Kiwi"), max.findMaximum(max));		
 	}
 }
