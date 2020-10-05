9a249ec00c42d10931437d1fa3e1d6b049e5d18f
Aditya Verma <aaadarths2357@gmail.com>
2020-10-05 20:29:58 +0530

    Refactor 1 : One Generic Method to find Maximum

--- src/main/java/com/bridgelabz/maximumprogram/Maximum.java
+++ src/main/java/com/bridgelabz/maximumprogram/Maximum.java
@@ -4,38 +4,14 @@ import java.lang.Comparable;
 
 public class Maximum {
 
-	public Integer findMaximumInteger(Integer[] integers) {
+	public <T extends Comparable<T>> T findMaximum(T a, T b, T c) {
 
-		Integer maxInt = integers[0];
-		if (integers[1].compareTo(maxInt) > 0)
-			maxInt = integers[1];
-		if (integers[2].compareTo(maxInt) > 0)
-			maxInt = integers[2];
-
-		return maxInt;
-	}
-
-	public Float findMaximumFloat(Float[] numbers) {
-
-		Float maxFloat = numbers[0];
-		if (numbers[1].compareTo(maxFloat) > 0)
-			maxFloat = numbers[1];
-		if (numbers[2].compareTo(maxFloat) > 0)
-			maxFloat = numbers[2];
-
-		return maxFloat;
-	}
-
-	public String findMaximumString(String[] sequence) {
-
-		String maxString = sequence[0];
-		if (sequence[1].compareTo(maxString) > 0)
-			maxString = sequence[1];
-		if (sequence[2].compareTo(maxString) > 0)
-			maxString = sequence[2];
-
-		return maxString;
-
+		T maxInput = a;
+		if (b.compareTo(maxInput) > 0)
+			maxInput = b;
+		if (c.compareTo(maxInput) > 0)
+			maxInput = c;
+		return maxInput;
 	}
 
 }
--- src/test/java/com/bridgelabz/maximumprogram/MaximumTest.java
+++ src/test/java/com/bridgelabz/maximumprogram/MaximumTest.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.maximumprogram;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,50 +10,53 @@ import junit.framework.Assert;
 public class MaximumTest {
 
 	Maximum max;
-
+	
 	@Before
 	public void initialize() {
-		max = new Maximum();
+		max=new Maximum();
 	}
 
-//INTEGER TEST	@Test
+	
+
+//INTEGER TEST	
+	@Test
 	public void givenMaxNumber_At1stPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(3), max.findMaximumInteger(new Integer[] {3,2,1}));		
+		Assert.assertEquals(new Integer(3), max.findMaximum(3, 2, 1));	
 	}	
 	@Test
 	public void givenMaxNumber_At2ndPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(4), max.findMaximumInteger(new Integer[] {3,4,1}));		
+		Assert.assertEquals(new Integer(4), max.findMaximum(3, 4, 1));		
 	}
 	@Test
 	public void givenMaxNumber_At3rdPosition_ReturnNumber() {
-		Assert.assertEquals(new Integer(5), max.findMaximumInteger(new Integer[] {3,4,5}));		
+		Assert.assertEquals(new Integer(5), max.findMaximum(3,4,5));		
 	}
 	
 //FLOAT TEST	
 	@Test
 	public void givenMaxFloat_At1stPosition_ReturnFloat() {
-		Assert.assertEquals(new Float(3f), max.findMaximumFloat(new Float[] {3f,2f,1f}));		
+		Assert.assertEquals(new Float(3f), max.findMaximum(3f,2f,1f));		
 	}	
 	@Test
 	public void givenMaxFloat_At2ndPosition_ReturnFloat() {
-		Assert.assertEquals(new Float(4f), max.findMaximumFloat(new Float[] {3f,4f,1f}));		
+		Assert.assertEquals(new Float(4f), max.findMaximum(3f,4f,1f));		
 	}
 	@Test
 	public void givenMaxFloat_At3Position_ReturnFloat() {
-		Assert.assertEquals(new Float(5f), max.findMaximumFloat(new Float[] {3f,4f,5f}));		
+		Assert.assertEquals(new Float(5f), max.findMaximum(3f,4f,5f));		
 	}
 
 //STRING TEST
 	@Test
 	public void givenMaxString_At1stPosition_ReturnString() {
-		Assert.assertEquals(new String("Cherries"), max.findMaximumString(new String[] {"Cherries","Banana","Apple"}));		
+		Assert.assertEquals(new String("Cherries"), max.findMaximum("Cherries","Banana","Apple"));		
 	}	
 	@Test
 	public void givenMaxString_At2ndPosition_ReturnString() {
-		Assert.assertEquals(new String("Grapefruit"), max.findMaximumString(new String[] {"Cherries","Grapefruit","Apple"}));		
+		Assert.assertEquals(new String("Grapefruit"), max.findMaximum("Cherries","Grapefruit","Apple"));		
 	}
 	@Test
 	public void givenMaxString_At3rdPosition_ReturnString() {
-		Assert.assertEquals(new String("Kiwi"), max.findMaximumString(new String[] {"Cherries","Banana","Kiwi"}));		
+		Assert.assertEquals(new String("Kiwi"), max.findMaximum("Cherries","Banana","Kiwi"));		
 	}
 }
